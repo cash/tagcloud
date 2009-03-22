6883163320b01352db884437e9a659a382d5da3b
cash <cash.costello@gmail.com>
2009-03-22 00:08:18 +0000

    added a function for displaying tag clouds to front page or riverdashboard

--- start.php
+++ start.php
@@ -1,5 +1,13 @@
 <?php
 
+  function frontpage_tagcloud($num_tags) {
+    $prev_context = get_context();
+    set_context('random');	
+	  $string = display_tagcloud(1, $num_tags, 'tags');
+	  set_context($prev_context);
+	  return $string;
+  }
+
   function tagcloud_init() {
     add_widget_type('tagcloud', elgg_echo('tagcloud:widget:title'), elgg_echo('tagcloud:widget:description'));			     
   }
