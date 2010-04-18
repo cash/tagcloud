1ac02b364d3ed622ea74654f48954f7c26fe2081
cash <cash.costello@gmail.com>
2010-04-18 21:11:08 +0000

    forgot the css from previous commit

---
+++ views/default/tagcloud/css.php
@@ -0,0 +1,43 @@
+/************* begin tag cloud ***************************/
+
+a.tagcloud {
+text-decoration: none;
+}
+
+a.tagcloud:hover {
+text-decoration: none;
+}
+
+a.tagcloud1 {
+color: red;
+}
+
+a.tagcloud1:hover {
+color: red;
+}
+
+a.tagcloud2 {
+color: black;
+}
+
+a.tagcloud2:hover {
+color: black;
+}
+
+a.tagcloud3 {
+color: green;
+}
+
+a.tagcloud3:hover {
+color: green;
+}
+
+a.tagcloud4 {
+color: brown;
+}
+
+a.tagcloud4:hover {
+color: brown;
+}
+
+/************** end tag cloud ****************************/
